9265ebee036b5f3c396e0312edd7ad944376c8e0
Daniel Majchrzak <dan.majchrzak@o2.pl>
2022-09-10 13:24:09 +0200

    (AppTeka)feat: dodanie RestAPI oraz bazy danych; (AppTekaClient)feat: stworzenie panelu logowania w kliencie

---
+++ ClientAppTeka/Operations.cs
@@ -0,0 +1,35 @@
+﻿using AppTeka.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClientAppTeka
+{
+    internal class Operations<T>
+    {
+        public List<T> elements;
+        private string name = typeof(T).Name.ToLower();
+
+        public async Task OnGet()
+        {
+            elements = await GetMyObjectAsync();
+        }
+
+        public async Task<List<T>> GetMyObjectAsync(CancellationToken cts = default)
+        {
+            var _httpClient = new HttpClient();
+            // http get request to a rest api address
+            var myObject = await _httpClient.GetFromJsonAsync<List<T>>($"https://localhost:5001/api/{name}", cts);
+
+            // raise error if deserialization was not possible
+            if (myObject == null)
+                throw new Exception("Oops... Something went wrong");
+            return myObject;
+        }
+    }
+}
